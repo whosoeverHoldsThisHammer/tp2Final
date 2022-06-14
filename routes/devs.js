86346fa46ebdd7b228b8aaa922372c72316ab195
whosoeverHoldsThisHammer <andresjfiorito@hotmail.com>
2022-06-13 21:42:03 -0300

    Métodos y rutas para Devs

--- routes/devs.js
+++ routes/devs.js
@@ -1,9 +1,22 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const data = require('./../data/devs');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', async(req, res, next) => {
+  const devs = await data.getDevs()
+  res.json(devs);
 });
+
+router.post('/', async(req, res) => {
+  const dev = req.body;
+  const result = await data.newDev(dev);
+  res.json(result)
+})
+
+router.put('/', async(req, res) => {
+  const dev = req.body;
+  const result = await data.updateDev(dev);
+  res.json(result)
+})
 
 module.exports = router;
