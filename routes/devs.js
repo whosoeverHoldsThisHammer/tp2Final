2b733bba782d4cf773ccee2940ef8f07fd137bb7
Cata Buljevich <catalinabuljevich@gmail.com>
2022-06-20 14:20:00 -0300

    user authentication

--- routes/devs.js
+++ routes/devs.js
@@ -1,28 +1,28 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const data = require('./../data/devs');
+const data = require("./../data/devs");
 
-router.get('/', async(req, res, next) => {
-  const devs = await data.getDevs()
+router.get("/", async (req, res, next) => {
+  const devs = await data.getDevs();
   res.json(devs);
-})
+});
 
-router.get('/:id', async(req, res) => {
+router.get("/:id", async (req, res) => {
   const id = req.params.id;
   const dev = await data.getDev(id);
   res.json(dev);
-})
+});
 
-router.post('/', async(req, res) => {
+router.post("/", async (req, res) => {
   const dev = req.body;
   const result = await data.newDev(dev);
-  res.json(result)
-})
+  res.json(result);
+});
 
-router.put('/', async(req, res) => {
+router.put("/", async (req, res) => {
   const dev = req.body;
   const result = await data.updateDev(dev);
-  res.json(result)
-})
+  res.json(result);
+});
 
 module.exports = router;
