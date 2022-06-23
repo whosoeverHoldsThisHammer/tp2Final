6ebdc51af67fd5d95be07e425e42b0b3443b872b
fsassy <federico.sassy@mercadolibre.com>
2022-06-23 18:40:27 -0300

    fix typo

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use("/", indexRouter);
 app.use("/api/devs", devsRouter);
 app.use("/api/projects", projectsRouter);
 app.use("/api/users", usersRouter);
-app.user("/api/managers", managsRouter);
+app.use("/api/managers", managsRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
